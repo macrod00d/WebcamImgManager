ac9b053d6efccbb47596cd440683409a3c591f03
Mishca <mishca.work@gmail.com>
2024-02-24 20:51:21 -0500

    refactor for module speration

--- app.py
+++ app.py
@@ -4,140 +4,86 @@ import numpy as np
 from datetime import datetime
 import os
 
-from sqlalchemy import Column, Integer, String, DateTime, create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy.dialects.sqlite import JSON
+from dbUtils import insert_image_metadata, init_db
+from components import details_form, capture_form
 
-engine = create_engine('sqlite:///image_metadata.db', echo=False)
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
-
-from typing import List
-def insert_image_metadata(title: str, description: List[str], filepath: str) -> None:
-    # Create a new session
-
-    session = Session()
-    try:
-        # Create a new ImageMetadataModel instance with the provided data
-        new_image_metadata = ImageMetadataModel(
-            title=title,
-            description=description,
-            filepath=filepath
-        )
-        
-        # Add the new instance to the session
-        session.add(new_image_metadata)
-        
-        # Commit the session to save the data to the database
-        session.commit()
-        print(f"Image metadata for '{title}' added successfully.")
-    except Exception as e:
-        # Rollback the session in case of error
-        session.rollback()
-        print(f"An error occurred: {e}")
-    finally:
-        # Close the session
-        session.close()
-
-class ImageMetadataModel(Base):
-    __tablename__ = 'image_metadata'
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    timestamp = Column(DateTime, default=datetime.utcnow)  # Timestamp is generated when a record is inserted
-    filepath = Column(String, nullable=False)
-
-# Function to save the image with a timestamp
 def save_image(cv2_img, base_path='./img'):
+    """
+    Save the OpenCV image to a file.
+
+    Args:
+        cv2_img (numpy.ndarray): The OpenCV image.
+        base_path (str, optional): The base path to save the image. Defaults to './img'.
+
+    Returns:
+        str: The path to the saved image.
+    """
     if not os.path.exists(base_path):
         os.makedirs(base_path)
-    
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
     filename = f"img_{timestamp}.png"
     save_path = os.path.join(base_path, filename)
     cv2.imwrite(save_path, cv2_img)
     return save_path
 
-# Initialize session state
-if 'page' not in st.session_state:
-    st.session_state['page'] = 'capture'
-
-def details_form():
-    st.subheader("Enter details for your image")
-    with st.form(key='details_form'):
-        # Initialize session state for title and description if not present
-        if 'title' not in st.session_state:
-            st.session_state['title'] = ''
-        if 'description' not in st.session_state:
-            st.session_state['description'] = ''
-
-        # Create text input fields within the form
-        title = st.text_input("Title", value=st.session_state['title'])
-        description = st.text_area("Description", value=st.session_state['description'])
-
-        # Create a form submit button
-        submitted = st.form_submit_button("Submit Details")
-        if submitted:
-            # Update session state with the latest input values
-            st.session_state['title'] = title
-            st.session_state['description'] = description
-
-            # Call the callback function to handle the form submission
-            submit_details_cb()
-
 def submit_details_cb():
-    # Retrieve title and description from st.session_state
+    """
+    Callback function for submitting image details.
+
+    This function retrieves the title and description from the session state,
+    inserts the image metadata into the database, and resets the session state.
+    """
     title = st.session_state.get('title', '')
     description = st.session_state.get('description', '')
-
-    # Continue with the database insertion using the retrieved values
     if 'image_path' in st.session_state:
-        insert_image_metadata(title, description, st.session_state['image_path'])
-        st.success(f"Image metadata for '{title}' added successfully.")
-        # Reset the form and go back to capture page
+        # Insert the image metadata into the database
+        try:
+            insert_image_metadata(title, description, st.session_state['image_path'])
+            st.success(f"Image metadata for '{title}' added successfully.")
+        except Exception as e:
+            st.error(f"Error inserting image metadata: {e}")
+
+        # Reset the session state
         st.session_state.page = 'capture'
-        # Optionally, clear the form inputs
         st.session_state['title'] = ''
         st.session_state['description'] = ''
-        st.rerun()
-
-def capture_form():
-    with st.form("capture_form"):
-        img_file_buffer = st.camera_input("Take a picture")
-
-        # The form_submit_button is the trigger for the form submission
-        submitted = st.form_submit_button("Save Image")
-
-        if submitted:
-            if img_file_buffer is not None:
-                # If an image has been taken and the button is clicked, call the save_image_cb
-                save_image_cb(img_file_buffer)
-            else:
-                # If no image has been taken, display a warning message
-                st.warning("Please take a picture before saving.")
+        st.rerun() #neded to ensure the session state is reset
 
 def save_image_cb(img_file_buffer):
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    """
+    Callback function for saving the captured image.
+
+    This function decodes the image file buffer, saves the image, and updates the session state.
     
-    save_path = save_image(cv2_img)
-    st.session_state['image_path'] = save_path  # Save path in session state
-    st.session_state.page = 'details'  # Change page to details form
-    st.experimental_rerun()
+    Args:
+        img_file_buffer (BytesIO): The image file buffer.
+    """
+    bytes_data = img_file_buffer.getvalue()
+    
+    try:
+        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    except Exception as e:
+        st.error(f"Error decoding image: {e}")
+    try:
+        save_path = save_image(cv2_img)
+    except Exception as e:
+        st.error(f"Error saving image: {e}")
+
+    # Update the session state
+    st.session_state['image_path'] = save_path
+    st.session_state.page = 'details'
+    st.rerun()
 
 def main():
-    # Initialize session state for page navigation if not present
     if 'page' not in st.session_state:
         st.session_state['page'] = 'capture'
-
-    # Main logic for page navigation
     if st.session_state.page == 'capture':
-        capture_form()
+        capture_form(save_image_cb)
     elif st.session_state.page == 'details':
         if 'image_path' in st.session_state:
             st.image(st.session_state['image_path'], caption="Captured Image", width=200)
-        details_form()
+        details_form(submit_details_cb)
 
 if __name__ == "__main__":
+    init_db()
     main()
---
+++ components.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+def details_form(submit_details_cb):
+    """
+    Renders form for entering details of image after image has been captured using capture_form.
+
+    Args:
+        submit_details_cb (function): Callback function to be executed when details are submitted.
+    """
+    st.subheader("Enter details for your image")
+    with st.form(key='details_form'):
+        title = st.text_input("Title", value=st.session_state.get('title', ''))
+        description = st.text_area("Description", value=st.session_state.get('description', ''))
+        submitted = st.form_submit_button("Submit Details")
+        if submitted:
+            if not title or not description:
+                if not title:
+                    st.warning('Title is required.')
+                if not description:
+                    st.warning('Description is required.')
+                st.stop()
+            else:
+                st.session_state['title'] = title
+                st.session_state['description'] = description
+                submit_details_cb()
+
+def capture_form(save_image_cb):
+    """
+    Renders form for capturing and saving an image using st.camera_input.
+
+    Args:
+        save_image_cb (function): Callback function to be executed when image is saved.
+    """
+    with st.form("capture_form"):
+        img_file_buffer = st.camera_input("Take a picture")
+        submitted = st.form_submit_button("Save Image")
+        if submitted:
+            if img_file_buffer is not None:
+                save_image_cb(img_file_buffer)
+            else:
+                st.warning("Please take a picture before saving.")
